79a4eef858a82e663054063234d8519184397c13
iratxe-pm <iratxepm04@gmail.com>
2025-11-07 20:27:29 +0100

    CSV

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.template import loader
-<<<<<<< HEAD
 from django.shortcuts import render
 
 from home.models import Producto
@@ -14,23 +13,5 @@ def index(request):
         'productos_destacados': listaproductos,
         
     }
-=======
-from .models import Producto 
-from django.shortcuts import render 
-
-def index(request):
-  
-    productos = Producto.objects.all()
-
-    if productos.exists():
-        producto = productos.first()
-        contexto = {
-            'nombre_articulo': producto.nombre,    
-        }    
-    else:
-        contexto = {
-            'nombre_articulo': 'No hay productos disponibles',
-        }
->>>>>>> 886e90c34da5004bc1824abd315461e58d8b2d44
     plantilla = loader.get_template('index.html')
     return HttpResponse(plantilla.render(contexto, request))
