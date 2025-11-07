f2ef1f3565ae8fb35292f5a5fa0a5e7ee70ddb81
lidiafc8 <lidiaestudios14@gmail.com>
2025-11-07 14:01:22 +0100

    Panel de administrador creado sin funcionalidades

--- mundo_jardin/urls.py
+++ mundo_jardin/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from home import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
+    path('adminpanel/', include('adminpanel.urls')),
 ]
 
 # BLOQUE CONDICIONAL AÑADIDO:
