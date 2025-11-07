34507b7f1557d72d8a4985ef31634d376757388c
iratxe-pm <iratxepm04@gmail.com>
2025-11-07 12:26:19 +0100

    front basico (incompleta funcionalidad)

--- home/views.py
+++ home/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from .models import Escaparate, Articulo
+
+from home.models import Producto
+
+
 
 def index(request):
-    escaparates = Escaparate.objects.all()
+    escaparates = Producto.objects.all()
     escaparate = escaparates.first()
-    articulos = Articulo.objects.filter(pk = escaparate.articulo.id)
-    articulo = articulos.first()
     contexto = {
-        'nombre_articulo': articulo.nombre,
+        'nombre_articulo': escaparate,
     }
     plantilla = loader.get_template('index.html')
     return HttpResponse(plantilla.render(contexto, request))
--- mundo_jardin/urls.py
+++ mundo_jardin/urls.py
@@ -17,9 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
-    
 ]
+
+# BLOQUE CONDICIONAL AÑADIDO:
+# Esto solo se ejecuta cuando DEBUG=True, permitiendo que el servidor local sirva estáticos.
+if settings.DEBUG:
+    # Mapea /static/ a la carpeta definida en STATICFILES_DIRS.
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+
+
