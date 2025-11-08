2afd69f9b52e376506a742d77998d639db83c892
iratxe-pm <iratxepm04@gmail.com>
2025-11-08 11:39:55 +0100

    css

--- mundo_jardin/urls.py
+++ mundo_jardin/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('subcategorias/<slug:categoria>/', views.index, name='productos_por_categoria'),    path('catalogoAdmin/', include('catalogo_admin.urls')),
+    path('subcategorias/<slug:categoria>/', views.index, name='productos_por_categoria'),    
+    path('catalogoAdmin/', include('catalogo_admin.urls')),
     
     path('adminpanel/', include('adminpanel.urls')),
     path('ventas_admin/', include('ventas_admin.urls')),
